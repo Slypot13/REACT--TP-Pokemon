e293d9dccff7e6e455d2e7a618a6216a50f9817e
Slypot <sylvain.pottier@ynov.com>
2026-09-02 20:25:05 +0200

    quete2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,13 @@
 function PokemonCard() {
   return (
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="Bulbasaur"
+      />
       <figcaption>Bulbasaur</figcaption>
     </figure>
   );
 }
+
+export default PokemonCard;
