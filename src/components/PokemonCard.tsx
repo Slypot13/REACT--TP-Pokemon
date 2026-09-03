e4f629a81bca65e05886ed401904d9697e60d1bc
Slypot <sylvain.pottier@ynov.com>
2026-09-03 18:48:09 +0200

    Quete 5 terminée

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,23 @@
+interface Pokemon {
+  name: string;
+  imgSrc?: string; // le ? rend le champ optionnel
+}
 
-function PokemonCard({pokemon}) {
+interface PokemonCardProps {
+  pokemon: Pokemon;
+}
+
+function PokemonCard({ pokemon }: PokemonCardProps) {
   return (
     <figure>
       {pokemon.imgSrc !== undefined ? (
-      <img
-        src={pokemon.imgSrc}
-        alt={pokemon.name}
-      /> ) : ( 
-      <p> "???" </p> )}
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
-
 
 export default PokemonCard;
